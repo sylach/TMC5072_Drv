5025e25d5ce1797812623a1a41368271c5aab861
Sylwester <sylwek.lach@in-motion.io>
2018-10-14 23:59:01 +0200

    ewwq

--- TMC5072_lib/tmc5072.c
+++ TMC5072_lib/tmc5072.c
@@ -28,7 +28,7 @@ static tmc5072_set_reg_struct tmc5072_set_dict[] = {
 	{"dmax1",	TMC5072_REG_DMAX_1},
 	{"dmax2",	TMC5072_REG_DMAX_2},
 	{"pos1",	TMC5072_REG_XTARGET_1},
-	{"pos2",	TMC5072_REG_XTARGET_2},
+	{"pos2",	TMC5072_REG_XTARGET_2as},
 	{"mode1",	TMC5072_REG_RAMPMODE_1},
 	{"mode2",	TMC5072_REG_RAMPMODE_2}
 };eew
